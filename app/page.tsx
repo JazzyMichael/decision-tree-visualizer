7fbdd1199776285326d38f31699305423e097692
Michael Mancini <thejazzymichael@gmail.com>
2026-09-02 19:49:45 -0400

    move canvas out of home page and update util linting

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,9 @@
-import { Background, Controls, Panel, ReactFlow } from "@xyflow/react";
-
-import PromptCard from "@/components/prompt-card";
-
 export default function Home() {
   return (
-    <main className="h-full w-full">
-      <ReactFlow>
-        <Panel position="bottom-center">
-          <PromptCard />
-        </Panel>
-        <Background />
-        <Controls />
-      </ReactFlow>
+    <main className="h-full w-full text-center pt-10">
+      <h2>(New tree form goes here)</h2>
+      <p className="my-10">...</p>
+      <h4>(Info about this app goes here)</h4>
     </main>
   );
 }
